12aa4f16f5a490a4d5522ad5db1817ebc958e733
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-09-16 09:33:57 +0200

    clobber!

--- reduction/stripfits.py
+++ reduction/stripfits.py
@@ -2,11 +2,11 @@ import glob
 from spectral_cube import SpectralCube
 from astropy import units as u
 
-for fn in glob.glob('h2comos*fits'):
+for fn in glob.glob('h2comos[0-6].fits'):
     cube = SpectralCube.read(fn)
     scube = cube.with_spectral_unit(u.GHz).with_spectral_unit(u.km/u.s, rest_value=4.82966*u.GHz, velocity_convention='radio')
     scube = scube.spectral_slab(-200*u.km/u.s, 200*u.km/u.s).minimal_subcube()
-    scube.write(fn.replace(".fits","_min.fits"))
+    scube.write(fn.replace(".fits","_min.fits"), overwrite=True)
 
 for fn in glob.glob('contmos[0-9].fits'):
     print(fn)
